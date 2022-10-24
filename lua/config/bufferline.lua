e1fcb0cd6ba35df9aa9252a80b18a10fc46c84c1
Gan Quan <imganquan@gmail.com>
2022-10-24 18:14:21 +0800

    Update bufferline.lua

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -6,8 +6,9 @@ require("bufferline").setup {
         left_mouse_command = "buffer %d",
         middle_mouse_command = nil,
         indicator = {
-            icon = "▎", -- this should be omitted if indicator style is not 'icon'
-            style = "icon",
+            -- icon = "▎", -- this should be omitted if indicator style is not 'icon'
+            -- style = "icon",
+            style = "underline",
         },
         buffer_close_icon = "",
         modified_icon = "●",
@@ -17,7 +18,7 @@ require("bufferline").setup {
         max_name_length = 18,
         max_prefix_length = 15,
         tab_size = 10,
-        diagnostics = false,
+        diagnostics = "nvim_lsp",
         custom_filter = function(bufnr)
             -- if the result is false, this buffer will be shown, otherwise, this
             -- buffer will be hidden.
@@ -33,7 +34,7 @@ require("bufferline").setup {
 
             return true
         end,
-        show_buffer_icons = false,
+        show_buffer_icons = true,
         show_buffer_close_icons = true,
         show_close_icon = true,
         show_tab_indicators = true,
@@ -53,7 +54,7 @@ require("bufferline").setup {
             {
                 filetype = "Vista",
                 text = "Tag",
-                highlight = "",
+                highlight = "Directory",
                 separator = true -- use a "true" to enable the default, or set your own character
             },
         }
