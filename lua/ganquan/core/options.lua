52cd199b3bdab60849f22acedc1f38833541e2b7
Gan Quan <imganquan@gmail.com>
2022-10-31 17:30:30 +0800

    remove force horizontal split window below.

--- lua/ganquan/core/options.lua
+++ lua/ganquan/core/options.lua
@@ -36,6 +36,5 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
