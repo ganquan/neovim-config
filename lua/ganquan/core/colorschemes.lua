db88195d1354213a832224c296a19d6c146672b5
Gan Quan <imganquan@gmail.com>
2023-06-25 12:18:18 +0800

    update colorschemes.lua

--- lua/ganquan/core/colorschemes.lua
+++ lua/ganquan/core/colorschemes.lua
@@ -1,8 +1,16 @@
-vim.g.gruvbox_material_enable_italic = 1
-vim.g.gruvbox_material_better_performance = 1
-
-
 -- according to gruvbox-material help
 -- set the termguicolors before set colorscheme
 vim.opt.termguicolors = true
-vim.cmd([[colorscheme gruvbox-material]])
+
+-- set the colorscheme
+local colorscheme = "gruvbox-material"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  return
+end
+
+-- configuration for  gruvbox-material
+vim.g.gruvbox_material_enable_italic = 1
+vim.g.gruvbox_material_better_performance = 1
+
