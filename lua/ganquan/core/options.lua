f5db2b48aaad6695ed2950da69655bd0badaa804
Gan Quan <imganquan@gmail.com>
2023-07-24 13:09:33 +0800

    set clip board on macOS only avoid nvim lagging on remote debian.

--- lua/ganquan/core/options.lua
+++ lua/ganquan/core/options.lua
@@ -32,7 +32,17 @@ opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+local uname = vim.loop.os_uname()
+_G.OS = uname.sysname
+_G.IS_MAC = OS == 'Darwin'
+_G.IS_LINUX = OS == 'Linux'
+_G.IS_WINDOWS = OS:find 'Windows' and true or false
+_G.IS_WSL = IS_LINUX and uname.release:find 'Microsoft' and true or false
+
+if _G.IS_MAC then
+    opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+end
+-- opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
