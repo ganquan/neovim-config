3084fc5a234c16ffb84474c4654215cd1730a923
Gan Quan <imganquan@gmail.com>
2024-01-12 08:52:21 +0800

    map 'o' key as toggle folder key in neo tree.

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,5 +1,10 @@
 return {
   require("neo-tree").setup({
+    window = {
+      mappings = {
+        ["o"] = "toggle_node",
+      },
+    },
     filesystem = {
       window = {
         mappings = {
